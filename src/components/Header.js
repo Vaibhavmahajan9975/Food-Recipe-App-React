05034c6c33c48f7084e838c46e04ed89abae64dd
VaibhavBhagawanMahaj <VaibhavBhagawanMahaj@ETRM-JUN22-02>
2026-08-21 15:36:23 +0530

    cd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 
-
-const Header=(props)=> {
-    return (
-     
-        <div className="jumbotron">
-            <h1 className="display-1">
-                <i className="material-icons brand-icon" >fastfood</i>Food Recipe</h1>
-              <div className="input-group w-50 mx-auto">
-                <input type="text" className="form-control" placeholder="Search Your Recipe...." value={props.search}
-                onChange={props.on}                />
-                <div className="input-group-append" >
-               <button className="btn btn-dark" onClick={props.onSearchClick}>  Search Recipes </button>
-                </div>
-               
-              </div>
+const Header = (props) => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-1">
+        <i className="material-icons brand-icon">fastfood</i>Food Recipe
+      </h1>
+      <div className="input-group w-50 mx-auto">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Your Recipe...."
+          value={props.search}
+          onChange={props.on}
+        />
+        <div className="input-group-append">
+          <button className="btn btn-dark" onClick={props.onSearchClick}>
+            {" "}
+            Search Recipe{" "}
+          </button>
         </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
