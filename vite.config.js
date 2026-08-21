34e6bca5d18ead24ef38f8f1782ed2e24d8df46e
VaibhavBhagawanMahaj <VaibhavBhagawanMahaj@ETRM-JUN22-02>
2026-08-21 17:37:28 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const pagesBase = repositoryName ? `/${repositoryName}/` : '/';
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? pagesBase : '/',
   plugins: [
     react({
       include: /\.(js|jsx|ts|tsx)$/
